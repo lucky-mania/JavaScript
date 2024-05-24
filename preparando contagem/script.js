8c663cc44b554da4b2528d489afa3328997393fc
lucky <joeaneto@gmail.com>
2024-05-23 22:04:55 -0300

    12

--- preparando contagem/script.js
+++ preparando contagem/script.js
@@ -3,15 +3,15 @@ function contar() {
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
-    
+    //lenght= se tiver 0 letras 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[Erro] faltam dados!')
         res.innerHTML ='Impossivel contar'
      }  else{
    res.innerHTML = 'contando:'
-   let i = Number(ini.value)
-   let f = Number(fim.value)
-   let p = Number(passo.value)
+   let i = Number(ini.value)//convertendo numero
+   let f = Number(fim.value)//convertendo numero
+   let p = Number(passo.value)//convertendo numero
    if(p <= 0) {
     window.alert('considerando passo 1')
     p = 1 
